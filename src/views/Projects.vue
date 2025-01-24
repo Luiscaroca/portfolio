878549af944adf5475981f21a337d31d01d42410
Luiscaroca <kako.ljca@gmail.com>
2025-01-24 02:43:21 -0300

    Lista vista de estudios, vista proyectos bajo construccion

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="projects">
     <h1>Projects page</h1>
+    <img
+      class="img-fluid mt-3"
+      alt="Website Under Construction"
+      src="../assets/Under_Construction.png"
+    />
   </div>
 </template>
--- src/views/Studies.vue
+++ src/views/Studies.vue
@@ -1,5 +1,281 @@
 <template>
   <div class="studies">
-    <h1>Studies page</h1>
+    <div class="container text-center mt-4">
+      <div class="row justify-content-center">
+        <div class="col-10">
+          <h2 class="mb-3">Estudios formales</h2>
+          <ul class="list-group">
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Diplomado en Big Data para la Gestión,</strong>
+                Universidad de los Andes
+              </div>
+              <span class="badge text-bg-success rounded-pill"
+                >2024 - Presente</span
+              >
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong
+                  >Diplomado en Marketing Digital y Gestión Estratégica,</strong
+                >
+                Instituto Profesional AIEP
+              </div>
+              <span class="badge text-bg-success rounded-pill">2024</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong
+                  >Diplomado en Crecimiento Digital de Negocios y Marca
+                  personal,</strong
+                >
+                Universidad de Playa Ancha
+              </div>
+              <span class="badge text-bg-success rounded-pill">2024</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Diplomado en Emprendimiento Estratégico,</strong>
+                IP Chile
+              </div>
+              <span class="badge text-bg-success rounded-pill">2024</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong
+                  >Diplomado en Diseño y Gestión Ágil de Proyectos,</strong
+                >
+                Universidad de Playa Ancha
+              </div>
+              <span class="badge text-bg-success rounded-pill">2024</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Diplomado en Diseño y Programación Web,</strong>
+                Instituto Profesional AIEP
+              </div>
+              <span class="badge text-bg-success rounded-pill">2024</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong
+                  >Diplomado en Fundamentos de la Programación y Desarrollo
+                  Web,</strong
+                >
+                Universidad de Playa Ancha
+              </div>
+              <span class="badge text-bg-success rounded-pill">2024</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong
+                  >Ingeniería en Ejecución en Computación e Informática,</strong
+                >
+                Universidad Católica del Maule, Talca
+              </div>
+              <span class="badge text-bg-success rounded-pill"
+                >2021 - 2023</span
+              >
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong
+                  >Técnico de Nivel Superior en Programación y Análisis de
+                  sistemas,</strong
+                >
+                CFT San Agustín, Linares
+              </div>
+              <span class="badge text-bg-success rounded-pill"
+                >2018 - 2021</span
+              >
+            </li>
+          </ul>
+          <h2 class="mt-3 mb-3">Cursos y certificaciones</h2>
+          <ul class="list-group">
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Acelerador de Carrera con Power BI,</strong>
+                ZAKIDATA
+              </div>
+              <span class="badge text-bg-success rounded-pill">2024</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Scrum Fundamentals Certified (SFC),</strong>
+                SCRUMstudy - Accreditation Body for Scrum and Agile
+              </div>
+              <span class="badge text-bg-success rounded-pill">2024</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Manejo de Extintores Portátiles,</strong>
+                MUTUAL de Seguridad
+              </div>
+              <span class="badge text-bg-success rounded-pill">2024</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Bases de GIT, GITHUB,</strong>
+                Desafío LATAM
+              </div>
+              <span class="badge text-bg-success rounded-pill">2024</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong
+                  >Reanimación Cardiopulmonar Básica y Uso de Desfibrilador en
+                  Caso de Emergencia,</strong
+                >
+                IZIED
+              </div>
+              <span class="badge text-bg-success rounded-pill">2024</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Introducción a Power BI,</strong>
+                Curso en línea SENCE
+              </div>
+              <span class="badge text-bg-success rounded-pill">2023</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong
+                  >Ciberseguridad en enternos de aprendizajes digitales,</strong
+                >
+                Curso en línea SENCE
+              </div>
+              <span class="badge text-bg-success rounded-pill">2023</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Principios Básicos de Big Data,</strong>
+                Curso en línea SENCE
+              </div>
+              <span class="badge text-bg-success rounded-pill">2023</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Introducción a UX y UI,</strong>
+                Curso en línea SENCE
+              </div>
+              <span class="badge text-bg-success rounded-pill">2023</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Diseño Web con HTML5 + CSS,</strong>
+                Curso en línea SENCE
+              </div>
+              <span class="badge text-bg-success rounded-pill">2023</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Comercializa y diseña tu videojuego,</strong>
+                Curso en línea SENCE
+              </div>
+              <span class="badge text-bg-success rounded-pill">2023</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Analista Web,</strong>
+                Curso en línea SENCE
+              </div>
+              <span class="badge text-bg-success rounded-pill">2023</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Aprende Wordpress,</strong>
+                Curso en línea SENCE
+              </div>
+              <span class="badge text-bg-success rounded-pill">2023</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>Introducción al Análisis de datos con Python,</strong>
+                Desafío LATAM
+              </div>
+              <span class="badge text-bg-success rounded-pill">2023</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong>SQL Interactivo,</strong>
+                Desafío LATAM
+              </div>
+              <span class="badge text-bg-success rounded-pill">2023</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong
+                  >Aprende a Planificar y Gestionar Estrategias Exitosas en
+                  Redes Sociales,</strong
+                >
+                Curso en línea SENCE
+              </div>
+              <span class="badge text-bg-success rounded-pill">2023</span>
+            </li>
+            <li
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="me-auto">
+                <strong
+                  >Scrum Foundation Professional Certificate (SFPC),</strong
+                >
+                CertiProf
+              </div>
+              <span class="badge text-bg-success rounded-pill">2020</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
