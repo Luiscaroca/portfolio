f9c8ba812e7ce1dc51413eb267e65ace32ba0ef7
Luiscaroca <kako.ljca@gmail.com>
2025-05-07 19:27:02 -0400

    Se agrego nueva experiencia a acordion de trayectoria

--- src/components/Accordion_Home.vue
+++ src/components/Accordion_Home.vue
@@ -121,6 +121,14 @@
                   <li
                     class="list-group-item d-flex justify-content-between align-items-center"
                   >
+                    <strong>Trabajo independiente</strong>
+                    <span class="badge text-bg-success rounded-pill"
+                      >2024 - 2025</span
+                    >
+                  </li>
+                  <li
+                    class="list-group-item d-flex justify-content-between align-items-center"
+                  >
                     <strong>Coordinador de carrera</strong>
                     <span class="badge text-bg-success rounded-pill">2024</span>
                   </li>
