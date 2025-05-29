29d8e8b721f71abca3985655731c98f0c8560163
Luis Joaquín Caroca Araya <lj.caroca@gmail.com>
2025-05-29 09:31:48 -0400

    Se agrego tarjeta de proyecto Level Up, se modificaron las tarjetas

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,25 +1,50 @@
 <template>
-  <div class="projects mt-5 ms-5 d-flex gap-4">
+  <div class="mt-5 row row-cols-1 row-cols-md-3 g-4">
     <!-- Inicio de una tarjeta -->
-    <div
-      class="card"
-      style="width: 18rem"
-    >
-      <img
-        src="../assets/projects_images/PMJ.png"
-        class="card-img-top"
-        alt="Logo María José Caroca"
-      />
-      <div class="card-body">
-        <h5 class="card-title">Psicóloga María José</h5>
-        <p class="card-text">
-          Sitio web [Landing Page] de psicóloga particular.
-        </p>
-        <a
-          href="https://luiscaroca.github.io/psicologa_maria_jose_caroca/"
-          class="btn btn-primary"
-          >Ingresar al sitio</a
-        >
+    <div class="col">
+      <div
+        class="card h-100 mx-auto"
+        style="width: 25rem"
+      >
+        <img
+          src="../assets/projects_images/PMJ.png"
+          class="card-img-top"
+          alt="Logo María José Caroca Araya"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Psicóloga María José</h5>
+          <p class="card-text">
+            Sitio web [Landing Page] de psicóloga particular.
+          </p>
+          <a
+            href="https://luiscaroca.github.io/psicologa_maria_jose_caroca/"
+            class="btn btn-primary mt-auto text-white"
+            >Ingresar al sitio</a
+          >
+        </div>
+      </div>
+    </div>
+    <!-- Fin de una tarjeta -->
+    <!-- Inicio de una tarjeta -->
+    <div class="col">
+      <div
+        class="card h-100 mx-auto"
+        style="width: 25rem"
+      >
+        <img
+          src="../assets/projects_images/LUP.png"
+          class="card-img-top"
+          alt="Logo Level Up Estampados"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Level Up Estampados</h5>
+          <p class="card-text">Sitio web para Level Up Estampados.</p>
+          <a
+            href="#"
+            class="btn btn-primary mt-auto text-white"
+            >Ingresar al sitio</a
+          >
+        </div>
       </div>
     </div>
     <!-- Fin de una tarjeta -->
